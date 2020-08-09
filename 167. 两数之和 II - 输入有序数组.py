9b051e96112d20ae4d4fb3e277e869315dbbbb1f
deanjingshui <dean031925@126.com>
2020-08-09 11:05:20 +0800

    两数之和II 二分查找

--- 167. 两数之和 II - 输入有序数组.py
+++ 167. 两数之和 II - 输入有序数组.py
@@ -19,7 +19,7 @@
 from typing import List
 
 
-class Solution:
+class Solution_1:
     """
     author:fenghao
     date:2020.8.8
@@ -32,9 +32,14 @@ class Solution:
         p_left = 0
         p_right = len(numbers) - 1
         while p_left < p_right:
-            if numbers[p_left] + numbers[p_right] == target:
+            # if numbers[p_left] + numbers[p_right] == target:
+            #     break
+            # elif numbers[p_left] + numbers[p_right] < target:
+            #     p_left += 1
+            total = numbers[p_left] + numbers[p_right]   # 抽离出来，减少重复计算量
+            if total == target:
                 break
-            elif numbers[p_left] + numbers[p_right] < target:
+            elif total < target:
                 p_left += 1
             else:
                 p_right -= 1
@@ -43,8 +48,38 @@ class Solution:
         return [p_left, p_right]
 
 
-numbers = [2, 7, 11, 15]
-target = 9
-my_sol = Solution()
+class Solution_2:
+    """
+    author:https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/solution/liang-shu-zhi-he-ii-shu-ru-you-xu-shu-zu-by-leet-2/
+    date:2020.8.8
+    思路：二分查找
+    时间复杂度：O(nlgn)
+    空间复杂度：O(1)
+    """
+    def binary_search(self, array: List, key):
+        # 模拟二分查找
+        try:
+            return array.index(key)
+        except ValueError:
+            return None
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for index in range(1, len(numbers)):
+            key = target - numbers[index]
+            # 避免重复计算
+            j = self.binary_search(numbers[:index], key)    # 为了避免重复寻找，在寻找第二个数时，只在第一个数的右侧（或左侧？）寻找。
+            if j is not None:
+                return [j+1, index+1]
+
+
+
+
+# numbers = [2, 7, 11, 15]
+# target = 9
+# numbers = [3, 3]
+# target = 6
+numbers = [2,3,4]
+target = 6
+my_sol = Solution_2()
 print(my_sol.twoSum(numbers, target))
 
