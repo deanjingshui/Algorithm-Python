ca8eb65c37e2a00283d4e2d4dc9b7452329e3366
deanjingshui <dean031925@126.com>
2020-08-16 17:46:25 +0800

    数组的度：官方解法

--- 哈希/697. 数组的度.py
+++ 哈希/697. 数组的度.py
@@ -73,10 +73,10 @@ class Solution_2:
     """
     def findShortestSubArray(self, nums: List[int]) -> int:
         """
-        第一个哈希表存储遍历元素的信息
+        第一个哈希表存储遍历元素的信息：首末次出现的索引、长度、出现频率
         {1：{'first_index':0 index, 'last_index':4, 'len': 4, 'freq': 2}
          2：{'first_index': 1, 'last_index': 6, 'len': 6, 'freq': 3}}
-         第二个哈希表存储整体数组的信息
+         第二个哈希表存储整体数组的信息（也可以不用哈希结构，直接用几个变量）：当前的度，度对应的数字、子数组的长度
          {'degree': 1, 'nums': [], 'len':[]}
         """
         nums_map = {}
@@ -90,12 +90,73 @@ class Solution_2:
                 nums_map[num]['freq'] += 1
             if degree_map['degree'] < nums_map[num]['freq']:
                 degree_map['degree'] = nums_map[num]['freq']
-                degree_map['nums'] = [num]
+                degree_map['nums'] = [num]    # 有更大的度，则直接覆盖
                 degree_map['len'] = [nums_map[num]['len']]
             elif degree_map['degree'] == nums_map[num]['freq']:
-                degree_map['nums'].append(num)
+                degree_map['nums'].append(num)   # 有相等的度，则追加
                 degree_map['len'].append(nums_map[num]['len'])
         return min(degree_map['len'])
+
+
+class Solution_3:
+    """
+    author:fenghao
+    date:2020.8.16
+    思路：
+        基于上一个方法
+        只是将第一个哈希表去除last_index字段、第二个哈希表去除nums字段
+    力扣测试耗时：124 ms
+    时间复杂度：O(n)
+    """
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        """
+        第一个哈希表存储遍历元素的信息
+        {1：{'first_index':0 index, 'len': 4, 'freq': 2}
+         2：{'first_index': 1, 'last_index': 6, 'len': 6, 'freq': 3}}
+         第二个哈希表存储整体数组的信息
+         {'degree': 1, 'len':[]}
+        """
+        nums_map = {}
+        degree_map = {'degree': 1, 'len':[]}
+        for index, num in enumerate(nums):
+            if nums_map.get(num) is None:
+                nums_map[num] = {'first_index': index, 'len': 1, 'freq': 1}
+            else:
+                nums_map[num]['len'] = index - nums_map[num]['first_index'] + 1
+                nums_map[num]['freq'] += 1
+            if degree_map['degree'] < nums_map[num]['freq']:
+                degree_map['degree'] = nums_map[num]['freq']
+                degree_map['len'] = [nums_map[num]['len']]
+            elif degree_map['degree'] == nums_map[num]['freq']:
+                degree_map['len'].append(nums_map[num]['len'])
+        return min(degree_map['len'])
+
+
+class Solution_4(object):
+    """
+    author:力扣官方
+    date:2020.8.16
+    思路：
+        哈希，用了三个哈希表分别存储了所有元素的首尾索引及频次
+        我的方法是用到一个“两层”的哈希，第一层的key是元素，第二层的key是该元素的信息（首末次索引等），官方解法通过三个“单层”的哈希实现更简练
+    力扣测试耗时：80 ms
+    时间复杂度：O(n)
+    """
+    def findShortestSubArray(self, nums):
+        left, right, count = {}, {}, {}
+        for i, x in enumerate(nums):
+            if x not in left: left[x] = i
+            right[x] = i
+            count[x] = count.get(x, 0) + 1
+
+        ans = len(nums)
+        degree = max(count.values())
+        for x in count:
+            if count[x] == degree:
+                ans = min(ans, right[x] - left[x] + 1)
+
+        return ans
+
 
 # nums = [1, 2, 2, 3, 1, 4, 2]
 nums = [1,2,2,3,1]
