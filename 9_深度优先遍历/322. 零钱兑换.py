e83cc9e1cf234f6fb5e4214bba188783a5e47552
deanjingshui <dean031925@126.com>
2021-03-31 09:57:45 +0800

    [modify] 零钱兑换

--- 9_深度优先遍历/322. 零钱兑换.py
+++ 9_深度优先遍历/322. 零钱兑换.py
@@ -1,5 +1,6 @@
 """
-给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+如果没有任何一种硬币组合能组成总金额，返回 -1。
 
 示例 1:
 
@@ -42,6 +43,35 @@ class Solution:
         return ret
 
 
+class Solution_dp:
+    """
+    author:fenghao
+    date:2021.3.31
+    思路：动态规划(自底向上)
+            dp[i]的含义：组成金额为i最少需要的硬币个数
+            状态转移方程：dp[i] = min(dp[i - {t|t属于coins}]) + 1
+    时间复杂度：O(n)
+    空间复杂度：O(n)
+    """
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [-1] * (amount + 1)
+        # base case
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+            if i in coins:
+                dp[i] = 1
+                continue
+            tmp = []
+            for coin in coins:
+                if i-coin > 0 and dp[i-coin] != -1:
+                    tmp.append(dp[i-coin])
+            if len(tmp) != 0:
+                dp[i] = min(tmp) + 1
+            else:
+                dp[i] = -1
+
+        return dp[amount]
 
 
 coins = [1, 2, 5]
