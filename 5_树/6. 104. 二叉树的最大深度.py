e8f100c459b31428850cce159a6fd5f3812ef716
deanjingshui <dean031925@126.com>
2021-04-02 17:59:45 +0800

    [modify]注释

--- 5_树/6. 104. 二叉树的最大深度.py
+++ 5_树/6. 104. 二叉树的最大深度.py
@@ -26,12 +26,12 @@ class TreeNode:
         self.right = None
 
 
-class Solution_recursive_dfs:
+class Solution_dfs:
     """
     date:2020.10.10
     author:fenghao + 力扣
     思路：
-        递归 DFS
+        深度优先遍历  递归
             dp[root]的含义：root为根节点的树的深度
             递归方程：
                 根节点的深度为左子树深度、右子树深度中的最大值，再加 1
@@ -56,7 +56,7 @@ class Solution_recursive_dfs:
         return max(left_depth, right_depth)
 
 
-class Solution_recursive_dfs_simplify:
+class Solution_dfs_simplify:
     """
     date:2020.10.10
     author:fenghao + 力扣
@@ -72,13 +72,48 @@ class Solution_recursive_dfs_simplify:
         return max(left_depth, right_depth) + 1
 
 
-from collections import deque
-class Solution_iterate_bfs:
+class Solution_dfs_bottom2top:
     """
-    date:2021.3.29
+    author:力扣官方leetbook   https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xefb4e/
+    date:2021.4.2
+    思路：深度优先遍历-递归-自底向上-模板
+    """
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)  # 先依次访问左右子树
+        right = self.maxDepth(root.right)
+        result = max(left, right) + 1  # 再“做事”--->后序遍历！
+        return result
+
+
+class Solution_dfs_top2bottom:
+    """
+    author:力扣官方leetbook  https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xefb4e/
+    date:2021.4.2
+    思路：深度优先遍历-递归-自底向下-模板
+         Me：自底向上，是需要传递参数的？
+    Me: 代码没看懂
+    """
+    def maxDepth(self, root: TreeNode) -> int:
+        result = self.dfs(root, 0)  # don't forget to initialize answer before call dfs
+        return result
+
+    def dfs(self, root, depth):
+        if not root:
+            return depth
+        left = self.dfs(root.left, depth+1)
+        right = self.dfs(root.right, depth+1)
+        return max(left, right)
+
+
+from collections import deque
+class Solution_bfs:
+    """
     author:力扣官方
+    date:2021.3.29
     思路：
-        迭代 BFS
+        广度优先遍历（层序遍历） 迭代
     时间复杂度：O(n)
     空间复杂度：O(m)  每一层节点的最大数量
     """
@@ -86,19 +121,19 @@ class Solution_iterate_bfs:
         if not root:
             return 0
 
-        result = 0
+        level = 0
         queue = deque()
         queue.append(root)
         while len(queue) != 0:
-            level_len = len(queue)  # 取出当前队列的长度
+            level_len = len(queue)  # 注：取出当前队列的长度
             for i in range(level_len):
                 node = queue.popleft()
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            result += 1
-        return result
+            level += 1
+        return level
 
 
 """
@@ -118,7 +153,9 @@ node_3.right = node_20
 node_20.left = node_15
 node_20.right = node_7
 
-my_sol = Solution_recursive_dfs_simplify()
+my_sol = Solution_dfs_simplify()
 print(my_sol.maxDepth(node_3))
-my_sol = Solution_iterate_bfs()
+my_sol = Solution_bfs()
+print(my_sol.maxDepth(node_3))
+my_sol = Solution_dfs_top2bottom()
 print(my_sol.maxDepth(node_3))
