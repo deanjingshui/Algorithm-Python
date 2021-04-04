97bc94a46380f6be1da8f7b3eb55e85259558dd1
deanjingshui <dean031925@126.com>
2021-04-04 17:10:43 +0800

    [add]找到所有数组中消失的数字

---
+++ 3_哈希/448. 找到所有数组中消失的数字.py
@@ -0,0 +1,34 @@
+"""
+给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+
+示例:
+输入:
+[4,3,2,7,8,2,3,1]
+
+输出:
+[5,6]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+"""
+from typing import List
+
+
+class Solution:
+    """
+    author:力扣官方
+    date:2021.4.4
+    思路：以数组的索引为哈希的key
+    时间：O(n)
+    空间：O(1)
+    """
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for num in nums:
+            x = (num - 1) % n
+            nums[x] += n
+
+        ret = [i + 1 for i, num in enumerate(nums) if num <= n]
+        return ret
