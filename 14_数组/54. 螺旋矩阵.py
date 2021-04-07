3f3d1d2c48fe15c09edafff566359fb4574b84fb
deanjingshui <dean031925@126.com>
2021-04-07 15:18:05 +0800

    [modify]注释

--- 14_数组/54. 螺旋矩阵.py
+++ 14_数组/54. 螺旋矩阵.py
@@ -21,18 +21,20 @@ class Solution:
             由右往左 n-1 次
             由下往上 m-2 次
             ...
-         注意：遍历完所有元素，及时跳出循环
+         注意：每次走完一个边，判断是否遍历完所有元素，及时跳出循环
+    时间：O(m*n)
+    空间：O(1)
     """
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         result = list()
-        row = len(matrix)
-        column = len(matrix[0])
-        column_steps = column      # 水平方向步进次数
-        row_steps = row - 1        # 竖直方向步进次数
+        rows = len(matrix)
+        columns = len(matrix[0])
+        column_steps = columns      # 水平方向步进次数
+        row_steps = rows - 1        # 竖直方向步进次数
         row_index = 0
         column_index = 0
         num = 0
-        sum = row * column
+        total = rows * columns
         while column_steps > 0 or row_steps > 0:
             # 由左往右
             if column_index == 0 and row_index == 0:
@@ -45,7 +47,7 @@ class Solution:
                     column_index += 1
                     result.append(matrix[row_index][column_index])
             num += column_steps
-            if num == sum:
+            if num == total:
                 break
             column_steps -= 1
             # 由上往下
@@ -53,7 +55,7 @@ class Solution:
                 row_index += 1
                 result.append(matrix[row_index][column_index])
             num += row_steps
-            if num == sum:
+            if num == total:
                 break
             row_steps -= 1
             # 由右往左
@@ -61,7 +63,7 @@ class Solution:
                 column_index -= 1
                 result.append(matrix[row_index][column_index])
             num += column_steps
-            if num == sum:
+            if num == total:
                 break
             column_steps -= 1
             # 由下往上
@@ -69,15 +71,53 @@ class Solution:
                 row_index -= 1
                 result.append(matrix[row_index][column_index])
             num += row_steps
-            if num == sum:
+            if num == total:
                 break
             row_steps -= 1
 
         return result
 
 
-# matrix = [[1,2,3],[4,5,6],[7,8,9]]
-matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-my_sol = Solution()
-print(my_sol.spiralOrder(matrix))
+class Solution_peel_apple:
+    """
+    athor:力扣网友
+    date:2021.4.7
+    思路：“削苹果”
+    """
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        while matrix:
+            res += matrix.pop(0)
+            matrix = list(zip(*matrix))[::-1]  # 巧用zip、[::-1]，将矩阵翻转90度，另外使用星号*解包运算简化了代码
+        return res
 
+
+class Solution_recursive:
+    """
+    athor:力扣网友+fenghao
+    date:2021.4.7
+    思路：递归
+    """
+    def helper(self, ):
+        pass
+
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        row = len(matrix)
+        column = len(matrix[0])
+        # base case
+        if row == 0:
+            return []
+
+        result = list()
+        result += matrix[0]
+        result += matrix
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+# my_sol = Solution()
+my_sol = Solution_peel_apple()
+print(my_sol.spiralOrder(matrix))
+# print(matrix)
+# print(*matrix)
+# print(list(zip(*matrix)))
