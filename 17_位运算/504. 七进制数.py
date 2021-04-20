6b0489addb99ef96956e8994b3e4d5e3c6e574ae
deanjingshui <dean031925@126.com>
2021-04-20 14:58:37 +0800

    [add]七进制运算

---
+++ 17_位运算/504. 七进制数.py
@@ -0,0 +1,38 @@
+"""
+给定一个整数，将其转化为7进制，并以字符串形式输出。
+
+示例 1:
+输入: 100
+输出: "202"
+
+示例 2:
+输入: -7
+输出: "-10"
+
+https://leetcode-cn.com/problems/base-7/
+"""
+
+
+class Solution:
+    """
+    author:fenghao
+    date:2021.4.20
+    """
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        result = ""
+        flag = 1 if num > 0 else -1
+        num = abs(num)
+        while num:
+            num, mod = divmod(num, 7)
+            result = str(mod) + result
+        return str(int(result)*flag)
+
+
+# num = 101
+num = 7
+# num = -7
+my_sol = Solution()
+print(my_sol.convertToBase7(num))
